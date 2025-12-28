b67186e96fa63e2e836eccc6738723a79b2cbf03
presstek2258 <aramirez2258@gmail.com>
2025-12-28 13:30:37 -0500

    pythonic list usage added

--- functional_programming/map_filter_reduce_loop.py
+++ functional_programming/map_filter_reduce_loop.py
@@ -16,13 +16,23 @@ print(f"{arr = }\n")
 # map applies a function to every element
 # return a list
 print(f"{list(map(lambda x: 2*x, arr)) = }")
-print(f"{list(map(lambda x: x-1, arr)) = }\n")
+print(f"{list(map(lambda x: x-1, arr)) = }")
+
+# can also been done with conditions:
+# generally preffered (unless lazy loading is desired)
+print(f"{[2*x for x in arr] = }")
+print(f"{[x-1 for x in arr] = }\n")
 
 # applies a condition to every element
 # drops those that dont meet the condition
 # returns a list
 print(f"{list(filter(lambda x: x>3, arr)) = }")
-print(f"{list(filter(lambda x: x%2==0, arr)) = }\n")
+print(f"{list(filter(lambda x: x%2==0, arr)) = }")
+
+# can also been done with conditions:
+# generally preffered (unless lazy loading is desired)
+print(f"{[x for x in arr if x>3] = }")
+print(f"{[x for x in arr if x%2==0] = }\n")
 
 # applies a function to every element sequentially
 # at each step x is the current value
